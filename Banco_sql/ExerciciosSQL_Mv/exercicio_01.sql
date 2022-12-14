af114ef49c891b2ad554eabdbe815fd06be1bfe4
BianchiDev <ifpebianchi@gmail.com>
2022-12-14 00:00:03 -0300

    Exercicios 01 e 02

---
+++ Banco_sql/ExerciciosSQL_Mv/exercicio_01.sql
@@ -0,0 +1,7 @@
+/* Fazer uma pesquisa que retorne o código de atendimento e a data de atendimento e a data de
+alta.*/
+select
+atendime.cd_atendime,
+atendime.dt_atemdime,
+atemdime.dt_alta,
+from atendime
