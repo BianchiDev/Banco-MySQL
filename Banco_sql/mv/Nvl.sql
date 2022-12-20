93bb1742882759df3e004e4e13406171952a742f
BianchiDev <ifpebianchi@gmail.com>
2022-12-20 09:06:26 -0300

    funções avançadas

---
+++ Banco_sql/mv/Nvl.sql
@@ -0,0 +1,11 @@
+/*NVL é utilizado para transformar um resultado nulo em outro não nulo;
+No exemplo abaixo, eu quero que todos os cd_prestador que são nulo retornem o numero 99999*/
+
+select
+atendime.cd_atendimento
+,atendime.dt_atendimento
+,Nvl(atendime.cd_prestador, 999999)
+from
+atendime
+where
+trunc(dt_atendimento) = to_date('23/08/2021', 'dd/mm/yyyy')
---
+++ Banco_sql/mv/funcoes_avancadas.sql
@@ -0,0 +1,25 @@
+/*Case 
+ * Ultizado quando precisamos tranformar um dado em outro ou classifica-los;
+ * Ultiliza a lógica IF-THEN-ELSE;
+ *EX:*/
+
+
+/*Total de atendimento do mês de janeiro de 2018, separados por tipo de atendimento. */
+
+select case when atendime.tp_atendimento = 'I' THEN 'Internacao'
+            when atendime.tp_atendimento = 'U'THEN'Urgencia'
+            when atendime.tp_atendimento = 'A' THEN'Ambulatorial'
+            when atendime.tp_atendimento = 'E' THEN 'externo'
+                ELSE'Nao cadastrado' end tipo_de_atend_case
+                Decode(atendime.tp_atendimento ,'u','Urgencia'
+                                                ,'I', 'Internacao'
+                                                ,'A', 'Ambulatorio'
+                                                ,'E', 'Externo'
+                                                , 'Nao cadastrado') tipo_de_atend_decode
+                
+    count(atendime.cd_atendime) total_atendimento
+    from 
+    atendime
+    where
+    to char(atendime.dt_atendimento, 'mm/yyyy') = '01/2018'
+    group by  atendime.tp_atendimento
