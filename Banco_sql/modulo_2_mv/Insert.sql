bb181ca0a433b9cddc563dedc4cac352d1e84ee4
BianchiDev <ifpebianchi@gmail.com>
2022-12-30 17:41:59 -0300

    Estudos de delete e update

---
+++ Banco_sql/modulo_2_mv/Insert.sql
@@ -0,0 +1,4 @@
+/*Insere dados de uma tabela
+Isere uma linha por vez;
+Se for inserir todas as colunas, não é necessário citar as colunas depois de escrever a tabela.
+Deve se executar um commit para efetivar a inserção ao final, ou rollback para desfazer a inserção */
